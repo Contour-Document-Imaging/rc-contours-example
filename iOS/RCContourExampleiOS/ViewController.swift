44426694c28425bef5d507de6c3fbc1089771688
Kapil Teotia <kapil.teotia@urbanft.com>
2025-03-28 14:17:16 +0530

    iOS example added

---
+++ iOS/RCContourExampleiOS/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  RCContourExampleiOS
+//
+//  Created by kapil teotia on 28/03/25.
+//
+
+import UIKit
+import RC_Contour_SDK
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func openSDK() {
+        var configuration = Configuration()
+        configuration.clientId = "<CLIENT ID>"
+        configuration.token = "<TOEKN>"
+        RCContour.initialize(configuration: configuration)
+        let rcContourVC = RCContour.getLaunchScreen()
+        let navigationController = UINavigationController(rootViewController: rcContourVC)
+        navigationController.modalPresentationStyle = .fullScreen
+        self.present(navigationController, animated: false)
+    }
+    
+    @IBAction func sdkButtonClicked(_ sender: UIButton) {
+        openSDK()
+    }
+}
+
