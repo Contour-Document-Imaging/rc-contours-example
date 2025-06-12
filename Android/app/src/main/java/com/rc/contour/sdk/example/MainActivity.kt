af1d7854127fda3bd9d547f29c247e76d959517e
Vivek Kumar Sahani <vsahani@uft-int.com>
2025-06-12 15:11:15 +0530

    Added edit text for input different params

--- Android/app/src/main/java/com/rc/contour/sdk/example/MainActivity.kt
+++ Android/app/src/main/java/com/rc/contour/sdk/example/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.rc.contour.sdk.example
 
+
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.contourdocumentimaging.sdk.contour_rc.callbacks.IContourResultListener
 import com.contourdocumentimaging.sdk.contour_rc.modules.RCContour
+import com.contourdocumentimaging.sdk.contour_rc.modules.base.model.EnvironmentType
 import com.contourdocumentimaging.sdk.contour_rc.modules.base.model.RCContourModel
+import com.rc.contour.sdk.example.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
 
     private lateinit var buttonOpenSdk: Button
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         buttonOpenSdk = findViewById(R.id.openSDK)
         buttonOpenSdk.setOnClickListener {
+
             val contourModel = RCContourModel()
-            contourModel.clientId = ""
-            contourModel.token = ""
-            contourModel.environmentId = ""
+            contourModel.clientId = binding.etClientId.text.toString().trim()
+            contourModel.token = binding.etToken.text.toString().trim()
+            contourModel.environmentType = getEnvironmentTypeFromEditText(binding.etEnvironmentType)
+            contourModel.environmentId = binding.etEnvironmentId.text.toString().trim()
             contourModel.isLocationEnabled = false
             RCContour.launchSdk(this, contourModel, object: IContourResultListener {
                 override fun onSDKClosed() {
@@ -28,6 +35,17 @@ class MainActivity: AppCompatActivity() {
                 }
             })
         }
+    }
 
+    private fun getEnvironmentTypeFromEditText(editText: EditText): EnvironmentType? {
+        val inputText = editText.text.toString().trim().uppercase()
+
+        return when (inputText) {
+            "QA" -> EnvironmentType.QA
+            "UAT" -> EnvironmentType.UAT
+            "DEV" -> EnvironmentType.DEV
+            "PPE" -> EnvironmentType.PPE
+            else -> null
+        }
     }
 }
