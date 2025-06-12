d25ccd1b5642f46852a96a83979d2739a65d3cdb
Kapil Teotia <kapil.teotia@urbanft.com>
2025-06-12 15:12:47 +0530

    Added input field to test different environment

--- iOS/RCContourExampleiOS/ViewController.swift
+++ iOS/RCContourExampleiOS/ViewController.swift
@@ -9,17 +9,23 @@ import UIKit
 import RC_Contour_SDK
 class ViewController: UIViewController {
     
+    @IBOutlet weak var environmentTypeTextField: UITextField!
+    @IBOutlet weak var tokenTypeTextField: UITextField!
+    @IBOutlet weak var environmentIdTextField: UITextField!
+    @IBOutlet weak var clientIdTextField: UITextField!
+    var configuration = Configuration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
     
     func openSDK() {
-        var configuration = Configuration()
-        configuration.clientId = "YOUR_CLIENT_ID"
-        configuration.token = "YOUR_TOKEN"
-        configuration.environmentId = "ENVIRONMENT_ID"
+        configuration.clientId = clientIdTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        configuration.token = tokenTypeTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        configuration.environmentId = environmentIdTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
         configuration.isLocationEnabled = false
+        setEnvironmentType()
         RCContour.initialize(configuration: configuration)
         let rcContourVC = RCContour.getLaunchScreen()
         let navigationController = UINavigationController(rootViewController: rcContourVC)
@@ -30,5 +36,28 @@ class ViewController: UIViewController {
     @IBAction func sdkButtonClicked(_ sender: UIButton) {
         openSDK()
     }
+    func setEnvironmentType() {
+        switch environmentTypeTextField.text {
+        case "qa":
+            configuration.environmentType = .qa
+        case "dev":
+            configuration.environmentType = .dev
+        case "uat":
+            configuration.environmentType = .uat
+        case "ppe":
+            configuration.environmentType = .ppe
+        default:
+            break
+        }
+    }
 }
 
+extension ViewController: UITextViewDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() // Dismiss the keyboard
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
---
+++ iOS/RCContourSDK.xcframework/ios-arm64/RC_Contour_SDK.framework/Headers/RC_Contour_SDK.h
@@ -0,0 +1,18 @@
+//
+//  RC_Contour_SDK.h
+//  RC-Contour-SDK
+//
+//  Created by Kapil on 05/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RC_Contour_SDK.
+FOUNDATION_EXPORT double RC_Contour_SDKVersionNumber;
+
+//! Project version string for RC_Contour_SDK.
+FOUNDATION_EXPORT const unsigned char RC_Contour_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RC_Contour_SDK/PublicHeader.h>
+
+
