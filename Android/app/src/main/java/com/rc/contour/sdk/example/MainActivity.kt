0f69457eeab9a8f8f40311037a2f3bba59f76af7
kapilurbanft <112612518+kapilurbanft@users.noreply.github.com>
2026-01-29 14:26:26 +0530

    Version updated to v1.4.0

--- Android/app/src/main/java/com/rc/contour/sdk/example/MainActivity.kt
+++ Android/app/src/main/java/com/rc/contour/sdk/example/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        RCContour.preInitSDK(applicationContext, "<CLIENT_ID>")
         buttonOpenSdk = findViewById(R.id.openSDK)
         buttonOpenSdk.setOnClickListener {
             val contourModel = RCContourModel()
